12a4aab62c5beeb7a0c4c5b2db0a74a7ca0037a8
thaim <thaim24@gmail.com>
2018-02-26 23:24:35 +0900

    update version

--- init.rb
+++ init.rb
@@ -7,5 +7,5 @@ require_dependency 'redmine_issue_view_count/issue_query_patch'
 Redmine::Plugin.register :redmine_issue_view_count do
   name 'Redmine Issue View Count Plugin'
   author 'thaim'
-  version '0.0.3'
+  version '1.0.0'
 end
